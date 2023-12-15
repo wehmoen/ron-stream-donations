0e15bc45f828fb131869491a404acd1c157ddb9c
Nico Wehmöller <mail@wehmoen.dev>
2023-12-15 17:45:41 +0100

    Add type for APIError Add ErrorBadSignature

--- src/errors.ts
+++ src/errors.ts
@@ -7,3 +7,14 @@ export const ErrorBadPayload: SocketError = {
     code: 1000,
     message: 'Bad payload'
 }
+
+export type APIError = {
+    code: number,
+    message: string,
+    details?: any
+}
+
+export const ErrorBadSignature: APIError = {
+    code: 6000,
+    message: 'Bad signature'
+}
